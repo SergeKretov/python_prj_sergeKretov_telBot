95dc7586dda7b3afd18db04be06f798802c34c0a
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-01-26 06:21:19 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=['text'])
+
+def StartDialog(message):
+    bot.send_message(message.chat.id, message.text)
+
+#RUN
+bot.polling(none_stop=True)
+
---
+++ config.py
@@ -0,0 +1,1 @@
+TOKEN = '862029754:AAFEch9ivNBD7i82E0TsS0DWhGw0DO0GM8c' #bot token from @botFather
