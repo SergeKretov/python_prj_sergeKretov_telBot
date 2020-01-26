4dc4fd390e0e07e3a8ddb413d9a0b611d6f8d321
SergeKretov <60303226+SergeKretov@users.noreply.github.com>
2020-01-26 09:49:44 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 TOKEN = os.environ.get('serge_kretov_tel_bot') #bot token from @botFather in config vars on heroku.com
 
-Bot = commands.Bot(command_prefix = ":")
+Bot = commands.Bot(command_prefix = "!")
 Bot.remove_command('help')
 
 @Bot.event
